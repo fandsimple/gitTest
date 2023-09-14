5a5befc3eabef4b1c72c2cfbf12e822b214744de
fanding <fanding@guoyutec.com>
2023-09-14 10:21:33 +0800

    keword_test

---
+++ codesec_test/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# @Time    : 2023/9/14 10:19 上午
+# @Author  : fanding
+# @FileName: __init__.py.py
---
+++ codesec_test/keword_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# @Time    : 2023/9/14 10:19 上午
+# @Author  : fanding
+# @FileName: keword_test.py
+
+
+keyword = "e1775f6c-f90a-4247-977f-134722313254"
+keyword2 = "128acede-3518-4fb3-82c2-e34a4eddf78b"
+keyword3 = "84ecd26f-42cf-4c83-b696-3b20b0b3a560"
+
+
+
+
+
